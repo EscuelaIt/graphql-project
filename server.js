7afab042ca5453ff4551aabbbe0cea5fde3def02
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2018-03-15 14:43:46 -0400

    ✨ create grahql server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
+const app = new express();
+const PORT = 4200;
+
+app.use(
+  '/graphql',
+  bodyParser.json(),
+  graphqlExpress({
+    schema: '',
+  })
+);
+
+app.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql'
+  })
+);
+
+app.listen(PORT, () => {
+  console.log('OK');
+});
